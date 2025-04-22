62c11921878f13ea44a3d1d2a83510e820c3d992
Jhonatan Popov <jhonatan.popov@gmail.com>
2025-04-22 12:00:43 -0300

    Frontend Update

--- frontend/src/utils/pdfGenerator.js
+++ frontend/src/utils/pdfGenerator.js
@@ -1,16 +1,4 @@
-// Biblioteca recomendada: jsPDF + autoTable (ainda não importadas aqui)
-
-export function gerarPDFFuncionario(dados) {
-  console.log("Gerando PDF de ponto para Funcionário CLT", dados)
-  // TODO: Integrar jsPDF para geração de relatório completo
-}
-
-export function gerarPDFMotorista(dados) {
-  console.log("Gerando PDF de jornada para Motorista CLT", dados)
-  // TODO: Incluir dados de status, horários e rotas
-}
-
-export function gerarPDFPJ(dados) {
-  console.log("Gerando PDF de fechamento para Prestador PJ", dados)
-  // TODO: Adicionar cálculo de comissão e resumo de serviços
+export function gerarFechamentoPDF(data) {
+  // Geração de PDF com jsPDF ou similar
+  console.log("Gerar PDF para:", data)
 }
